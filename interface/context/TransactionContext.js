d42a76e93cb3d26e3d1c75bfd758645324b4750f
tnkshuuhei <shuhei46491@gmail.com>
2022-10-26 02:38:10 +0900

    fix context

--- interface/context/TransactionContext.js
+++ interface/context/TransactionContext.js
@@ -1,5 +1,5 @@
 import React ,{ useState,useEffect} from 'react'
-//import { WsProvider } from '@polkadot/api';
+//import { ApiPromise, WsProvider } from '@polkadot/api';
 //import dynamic from 'next/dynamic'
 //const web3Accounts = dynamic(() => import('@polkadot/extension-dapp'), { ssr: false })
 //const web3Enable = dynamic(() => import('@polkadot/extension-dapp'), { ssr: false })
@@ -11,6 +11,8 @@ let pjs
 if (typeof window !== 'undefined') {
   pjs = window.injectedWeb3
 }
+const WS_PROVIDER = 'ws://127.0.0.1:9944'
+const DAPP_NAME = 'Shiden DEX'
  export const TransactionProvider = ({children}) => {
 	const [currentAccount, setCurrentAccount] = useState()
 
@@ -23,16 +25,16 @@ if (typeof window !== 'undefined') {
 			if (!pjs) return alert('Please install polkadot-js ')
 			const { web3Enable,web3Accounts, web3FromSource } = await import("@polkadot/extension-dapp");
 
-			const extensions = await web3Enable('Shiden DEX');
+			const extensions = await web3Enable(DAPP_NAME);
 			if (extensions.length === 0) {
 				// no extension installed, or the user did not accept the authorization
 				// in this case we should inform the use and give a link to the extension
 				return;
 			}
 			const allaccounts = await web3Accounts();
-			setCurrentAccount(allaccounts[0])
-			//const injector = await web3FromSource(allaccounts.meta.source);
-			//console.log(injector);
+			const account = allAccounts[0];
+			setCurrentAccount(account)
+			const injector = await web3FromSource(account.meta.source);
 
 		  if (!pjs) return alert('Please install polkadot-js ')
 		} catch (error) {
@@ -43,7 +45,7 @@ if (typeof window !== 'undefined') {
     try {
 		const { web3Enable ,web3Accounts } = await import("@polkadot/extension-dapp");
 
-		const extensions = await web3Enable('Shiden DEX');
+		const extensions = await web3Enable(DAPP_NAME);
 		if (extensions.length === 0) {
 			// no extension installed, or the user did not accept the authorization
 			// in this case we should inform the use and give a link to the extension
