683924e5ab14692c303a1675e68dc29818d30072
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-10-27 10:42:15 +0900

    add API promise

--- interface/context/TransactionContext.js
+++ interface/context/TransactionContext.js
@@ -1,3 +1,4 @@
+import { ApiPromise, WsProvider } from '@polkadot/api'
 import React ,{ useState,useEffect} from 'react'
 //import { ApiPromise, WsProvider } from '@polkadot/api';
 //import dynamic from 'next/dynamic'
@@ -11,7 +12,7 @@ let pjs
 if (typeof window !== 'undefined') {
   pjs = window.injectedWeb3
 }
-const WS_PROVIDER = 'ws://127.0.0.1:9944'
+const WS_PROVIDER = 'wss://shibuya.public.blastapi.io'
 const DAPP_NAME = 'Shiden DEX'
  export const TransactionProvider = ({children}) => {
 	const [currentAccount, setCurrentAccount] = useState()
@@ -24,13 +25,22 @@ const DAPP_NAME = 'Shiden DEX'
 		try {
 			if (!pjs) return alert('Please install polkadot-js ')
 			const { web3Enable,web3Accounts, web3FromSource } = await import("@polkadot/extension-dapp");
-
 			const extensions = await web3Enable(DAPP_NAME);
 			if (extensions.length === 0) {
 				// no extension installed, or the user did not accept the authorization
 				// in this case we should inform the use and give a link to the extension
 				return;
 			}
+			const provider = new WsProvider(WS_PROVIDER);
+			// Create the API and wait until ready
+			const api = await ApiPromise.create({ provider });
+			const [chain, nodeName, nodeVersion] = await Promise.all([
+				api.rpc.system.chain(),
+				api.rpc.system.name(),
+				api.rpc.system.version()
+			  ]);
+			console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+			
 			const allaccounts = await web3Accounts();
 			const account = allAccounts[0];
 			setCurrentAccount(account)
@@ -44,7 +54,6 @@ const DAPP_NAME = 'Shiden DEX'
   const checkIfWalletIsConnected = async () => {
     try {
 		const { web3Enable ,web3Accounts } = await import("@polkadot/extension-dapp");
-
 		const extensions = await web3Enable(DAPP_NAME);
 		if (extensions.length === 0) {
 			// no extension installed, or the user did not accept the authorization
@@ -57,6 +66,15 @@ const DAPP_NAME = 'Shiden DEX'
         setCurrentAccount(allaccounts[0])
 		console.log('wallet is already connected!')
       }
+	  const provider = new WsProvider(WS_PROVIDER);
+	  // Create the API and wait until ready
+	  const api = await ApiPromise.create({ provider });
+	  const [chain, nodeName, nodeVersion] = await Promise.all([
+		  api.rpc.system.chain(),
+		  api.rpc.system.name(),
+		  api.rpc.system.version()
+		]);
+	  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
     } catch (error) {
       console.error(error)
     }
