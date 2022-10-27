cbbbbd3ba73e00e67c21add6742f76ec53651088
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-10-27 11:26:09 +0900

    add signer

--- interface/context/TransactionContext.js
+++ interface/context/TransactionContext.js
@@ -30,17 +30,9 @@ const DAPP_NAME = 'Shiden DEX'
 			const provider = new WsProvider(WS_PROVIDER);
 			// Create the API and wait until ready
 			const api = await ApiPromise.create({ provider });
-			const [chain, nodeName, nodeVersion] = await Promise.all([
-				api.rpc.system.chain(),
-				api.rpc.system.name(),
-				api.rpc.system.version()
-			]);
-			console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
-
 			const allaccounts = await web3Accounts();
 			const account = allaccounts[0];
 			setCurrentAccount(account)
-			
 		  if (!pjs) return alert('Please install polkadot-js ')
 		} catch (error) {
 			console.error(error)
@@ -48,27 +40,30 @@ const DAPP_NAME = 'Shiden DEX'
 	}
   const checkIfWalletIsConnected = async () => {
     try {
-		const { web3Enable ,web3Accounts } = await import("@polkadot/extension-dapp");
+		const { web3Enable ,web3Accounts,web3FromSource } = await import("@polkadot/extension-dapp");
 		const extensions = await web3Enable(DAPP_NAME);
 		if (extensions.length === 0) {
 			// no extension installed, or the user did not accept the authorization
 			// in this case we should inform the use and give a link to the extension
 			return;
 		}
-	  const allaccounts = await web3Accounts();
-
-      if (allaccounts.length) {
-        setCurrentAccount(allaccounts[0])
-      }
-	  const provider = new WsProvider(WS_PROVIDER);
-	  // Create the API and wait until ready
-	  const api = await ApiPromise.create({ provider });
-	  const [chain, nodeName, nodeVersion] = await Promise.all([
-		  api.rpc.system.chain(),
-		  api.rpc.system.name(),
-		  api.rpc.system.version()
-		]);
-	  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+		const allaccounts = await web3Accounts();
+		if (allaccounts.length) {
+			setCurrentAccount(allaccounts[0])
+		}
+		const provider = new WsProvider(WS_PROVIDER);
+		// Create the API and wait until ready
+		const api = await ApiPromise.create({ provider });
+		const account = allaccounts[0];
+		setCurrentAccount(account)
+		const transferExtrinsic = api.tx.balances.transfer('5C5555yEXUcmEJ5kkcCMvdZjUo7NGJiQJMS7vZXEeoMhj3VQ', 12345)
+		const injector = await web3FromSource(account.meta.source);
+		transferExtrinsic.signAndSend(account.address, { signer: injector.signer }, ({ status }) => {
+			if (status.isInBlock) {
+				console.log(`Completed at block hash #${status.asInBlock.toString()}`);
+			} else {
+				console.log(`Current status: ${status.type}`);
+			}})
     } catch (error) {
       console.error(error)
     }
