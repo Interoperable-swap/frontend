a57beb1805a1ce8f2a2194560718d33a785e2421
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-10-27 22:15:53 +0900

    AiOutlinePlus

--- interface/components/AddLiquidity.js
+++ interface/components/AddLiquidity.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { RiSettings3Fill } from 'react-icons/ri'
-import { AiOutlineDown } from 'react-icons/ai'
+import { AiOutlineDown, AiOutlinePlus } from 'react-icons/ai'
 import astar from '../assets/astar.png'
 import Shiden from '../assets/Shiden.png'
 import { ApiPromise, WsProvider } from "@polkadot/api";
@@ -18,6 +18,7 @@ const style = {
   currencySelectorIcon: `flex items-center`,
   currencySelectorTicker: `mx-2`,
   currencySelectorArrow: `text-lg`,
+  plusIcon: `flex items-center justify-center`,
   confirmButton: `bg-[#2172E5] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
 }
 const Liquidity = () => {
@@ -57,8 +58,12 @@ const Liquidity = () => {
 			  </div>
 			</div>
 		  </div>
-		  
-		{/*add plus Icon */}		  
+		  <div className={style.plusIcon}>		  
+			<AiOutlinePlus
+				size={42}
+			/>
+		  </div>
+		
 		  <div className={style.transferPropContainer}>
 			<input
 			  type='text'
