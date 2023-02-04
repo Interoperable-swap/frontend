f7f28cbee21feaca8c6ab30ad115987442da01eb
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-02-05 02:09:52 +0900

    add positon

--- interface/components/AccountList.js
+++ interface/components/AccountList.js
@@ -1,23 +1,51 @@
-import React, { useContext, useEffect } from 'react'
-import { TransactionContext } from '../context/TransactionContext'
-
-const style = {
-  wrapper: `w-screen flex items-center justify-center mt-14`,
-  content: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
-}
-
+import React, { useState } from 'react'
+import Card from './LP/Card'
+import PositionList from './LP/PositionList'
+import Empty_position from './LP/EmptyPosition'
 const AccountList = () => {
-  const { currentAccount } = useContext(TransactionContext)
+  const [positon, setPosition] = useState(false)
   return (
-    <div className={style.wrapper}>
-      {currentAccount ? (
-        <div className={style.content}>
-          <div className={style.buttonTextContainer}>Connected Address : {currentAccount.address}</div>
-        </div>
-      ) : (
-        <h1>Wallet is not connected</h1>
-      )}
+    <div>
+      <Card
+        button={
+          <button className='bg-blue-500 text-white py-2 px-4 rounded-full' onClick={() => setPosition(!positon)}>
+            New Position
+          </button>
+        }
+      >
+        {positon ? (
+          <div>
+            <PositionList
+              token0_symbol={'/assets/usdt.svg'}
+              token1_symbol={'/assets/astar.png'}
+              token0={'USDT'}
+              token1={'ASTR'}
+              rangerow={'0.05'}
+              rangehigh={'0.06'}
+            />
+            <PositionList
+              token0_symbol={'/assets/usdt.svg'}
+              token1_symbol={'/assets/uniswap.png'}
+              token0={'USDT'}
+              token1={'UNI'}
+              rangerow={'5.6'}
+              rangehigh={'6.4'}
+            />
+            <PositionList
+              token0_symbol={'/assets/uniswap.png'}
+              token1_symbol={'/assets/astar.png'}
+              token0={'UNI'}
+              token1={'ASTR'}
+              rangerow={'10.0'}
+              rangehigh={'12.3'}
+            />
+          </div>
+        ) : (
+          <Empty_position />
+        )}
+      </Card>
     </div>
   )
 }
+
 export default AccountList
