b48858da553f125f968406cabbe1bee3f6bbba83
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-02-13 15:13:08 +0900

    fix css

--- interface/components/LP/Pool.js
+++ interface/components/LP/Pool.js
@@ -8,26 +8,28 @@ const Pool = (props) => {
     <Link href={'/addliquidity'}>
       <div className='bg-gray-800  hover:bg-gray-700 p-5 rounded-2xl shadow-lg border border-gray-700 text-white cursor-pointer mb-4'>
         <div className='flex font-medium'>
-          <div className='w-1/5 py-3'>
-            <span className='mr-2'>
-              <Image src={token0_symbol} alt='token0' width={14} height={14} />
-              <Image src={token1_symbol} alt='token1' width={14} height={14} />
-            </span>
-            <span>
+          <div className='w-1/5 flex p-3'>
+            <div>
+              <span className='p-1 flex'>
+                <Image src={token0_symbol} alt='token0' width={16} height={16} />
+                <Image src={token1_symbol} alt='token1' width={16} height={16} />
+              </span>
+            </div>
+            <div>
               {token0} / {token1}
-            </span>
+            </div>
           </div>
           <div className='w-1/5 py-3'>
-            <span className='ml-4'>{TVL} K</span>
+            <span className='text-center'>{TVL} K</span>
           </div>
           <div className='w-1/5 py-3'>
-            <span className='ml-4'>${DVL} K</span>
+            <span className='text-center'>${DVL} K</span>
           </div>
           <div className='w-1/5 py-3'>
-            <span className='ml-4'>${Fee} K</span>
+            <span className='text-center'>${Fee} K</span>
           </div>
           <div className='w-1/5 py-3'>
-            <span className='ml-4'>${APY} %</span>
+            <span className=' text-center'>${APY} %</span>
           </div>
         </div>
       </div>
--- interface/components/positons/Position.js
+++ interface/components/positons/Position.js
@@ -6,14 +6,16 @@ const PositionList = (props) => {
 
   return (
     <div className='bg-gray-800 p-6 rounded-2xl shadow-lg border border-gray-700 text-white m-4 hover:bg-gray-700 cursor-pointer'>
-      <div className='font-medium text-left d-flex'>
-        <span>
-          <Image src={token0_symbol} alt='token0' width={16} height={16} />
-        </span>
-        <span className='mr-2'>
-          <Image src={token1_symbol} alt='token1' width={16} height={16} />
-        </span>
-        {token0} / {token1}
+      <div className='flex'>
+        <div className='p-1'>
+          <span className='flex'>
+            <Image src={token0_symbol} alt='token0' width={16} height={16} />
+            <Image src={token1_symbol} alt='token1' width={16} height={16} />
+          </span>
+        </div>
+        <div className='font-medium'>
+          {token0} / {token1}
+        </div>
       </div>
       <div className='font-medium text-left '>
         Min : {rangerow} {token0} per {token1} ↔︎ Max : {rangehigh} {token0} per {token1}
